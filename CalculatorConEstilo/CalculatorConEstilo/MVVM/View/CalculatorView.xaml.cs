dfff06e18c9979595c8c9d0c04515cfb7b9540e5
Carlos Orjuela <ingkrlosorjuela@evertecinc.com>
2023-01-20 17:57:16 -0500

    Termina Sesión 17

---
+++ CalculatorConEstilo/CalculatorConEstilo/MVVM/View/CalculatorView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using CalculatorConEstilo.MVVM.ViewModel;
+
+namespace CalculatorConEstilo.MVVM.View;
+
+public partial class CalculatorView : ContentPage
+{
+	public CalculatorView()
+	{
+		InitializeComponent();
+		BindingContext = new CalculatorViewModel();
+
+    }
+}
---
+++ CalculatorConEstilo/CalculatorConEstilo/MVVM/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+using Dangl.Calculator;
+using PropertyChanged;
+
+namespace CalculatorConEstilo.MVVM.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class CalculatorViewModel
+    {
+
+        public string Formula { get; set; }
+        public string Result { get; set; } = "0";
+
+        public ICommand OperationCommand => new Command((number) => Formula += number);
+        public ICommand ResetCommand => new Command((number) =>
+                                            {
+                                                Result = "0";
+                                                Formula = string.Empty;
+                                            });
+        public ICommand BackspaceCommand => new Command((number) =>
+        {
+            if (Formula.Length > 0)
+            {
+                Formula = Formula.Substring(0, Formula.Length - 1);
+            }
+        });
+
+        public ICommand CalculeteCommand => new Command((number) =>
+        {
+            if (Formula.Length == 0)
+                return;
+
+            var calculate = Calculator.Calculate(Formula);
+            Result = calculate.Result.ToString();
+        });
+
+        public CalculatorViewModel()
+        {
+        }
+    }
+}
+
